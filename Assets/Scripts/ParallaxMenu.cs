8e3892b53db1a164f7b06da4a13d89658284da9e
Nomsies <thopek007@gmail.com>
2023-12-28 18:32:47 +0700

    fix main menu & pause

---
+++ Assets/Scripts/ParallaxMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxMenu : MonoBehaviour
+{
+    public float scrollSpeed = 0.5f;
+    private float startpos, length;
+    //[SerializeField] private GameObject camera;
+
+    private void Start()
+    {
+        //startpos = transform.position.x;
+        //length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update()
+    {
+        float number = 0f;
+        //float temp = number * (1 - scrollSpeed);
+        float offset = number * scrollSpeed;
+
+        //backgroundRenderer.transform.position = new Vector3(offset, backgroundRenderer.transform.position.y, backgroundRenderer.transform.position.z);
+        transform.position = new Vector3(offset, transform.position.y, transform.position.z);
+
+        //if (temp > startpos + length)
+        //{
+        //    startpos += length;
+        //}
+        //else if (temp < startpos - length)
+        //{
+        //    startpos -= length;
+        //}
+
+        number++;
+    }
+}
